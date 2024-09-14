bf57db3a7ff34937a9acda2a2ba12d5f421bfb46
Varun1802 <60618498+Varun1802@users.noreply.github.com>
2024-09-15 01:15:19 +0530

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 import Header from "@/components/Header";
 import ViewCanvas from "@/components/ViewCanvas";
+import Footer from "@/components/Footer";
 // import Footer from "@/components/Footer";
 
 const alpino = localFont({
@@ -28,7 +29,7 @@ export default function RootLayout({
           {children}
           <ViewCanvas />
         </main>
-        {/* <Footer /> */}
+        <Footer />
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
